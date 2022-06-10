95c87113725978c2eacdc6964039f8269d8b91c8
matthew <mxy@spacetech.com.cn>
2022-06-10 16:41:35 +0800

    feat: 改动

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '魔晶博客', // 网站标题
   description:
     '魔晶代表有一种魔力深深吸引着每个人，但原来到此网站的所有人都有收货.', //网站描述
-  base: '/', //  部署时的路径 默认 /  可以使用二级地址 /base/
+  base: '/matthew-blog/', //  部署时的路径 默认 /  可以使用二级地址 /base/
   // lang: 'en-US', //语言
   // 打包目录
   dest: './dist',
@@ -17,15 +17,103 @@ module.exports = {
       },
     ],
   ],
+  // 使用插件
+  plugins: [
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top',
+    '@vuepress/medium-zoom',
+    '@vuepress/nprogress',
+    [
+      '@vuepress/plugin-search',
+      {
+        locales: {
+          '/': {
+            placeholder: 'Search',
+          },
+          '/zh/': {
+            placeholder: '搜索',
+          },
+        },
+      },
+    ],
+  ],
   // 主题配置
   themeConfig: {
-    repo: 'vuejs/vitepress', // 你的 github 仓库地址，网页的右上角会跳转
-    //   头部导航
+    // 获取每个文件最后一次 git 提交的 UNIX 时间戳(ms)，同时它将以合适的日期格式显示在每一页的底部
+    // lastUpdated: 'Last Updated', // string | boolean
+    // 启动页面丝滑滚动
+    smoothScroll: true,
+    // 导航栏配置
     nav: [
-      { text: '首页', link: '/' },
-      { text: '关于', link: '/about/' },
+      { text: '我的个人网站', link: '' },
+      { text: '掘金', link: '' },
+      { text: 'Github', link: '' },
     ],
-    //   侧边导航
-    sidebar: [{ text: '我的', link: '/mine/' }],
+    sidebar: {
+      '/': getSidebar(),
+    },
   },
+}
+
+function getSidebar() {
+  return [
+    {
+      text: 'HTML',
+      children: [
+        { text: '基础', link: '/HTML/' },
+        { text: '进阶', link: '/HTML/advanced' },
+      ],
+      sidebarDepth: 3,
+    },
+    {
+      text: 'CSS',
+      children: [
+        { text: '基础', link: '/CSS/' },
+        { text: '进阶', link: '/CSS/advanced' },
+      ],
+    },
+    {
+      text: 'Javascript',
+      children: [
+        { text: '基础', link: '/Javascript/' },
+        { text: '进阶', link: '/Javascript/advanced' },
+        { text: '进阶', link: '/Javascript/nightmare' },
+      ],
+    },
+    {
+      text: 'Vue',
+      children: [
+        { text: '基础', link: '/Vue/' },
+        { text: '进阶', link: '/Vue/advanced' },
+      ],
+    },
+    {
+      text: '浏览器',
+      children: [
+        { text: '基础', link: '/Vue/' },
+        { text: '进阶', link: '/Vue/advanced' },
+      ],
+    },
+    {
+      text: '网络',
+      children: [
+        { text: '基础', link: '/Network/' },
+        { text: '进阶', link: '/Network/advanced' },
+      ],
+    },
+    {
+      text: '安全',
+      children: [
+        { text: '基础', link: '/Security/' },
+        { text: '进阶', link: '/Security/advanced' },
+      ],
+    },
+    {
+      text: '面经',
+      children: [
+        { text: '基础', link: '/Experience/' },
+        { text: '进阶', link: '/Experience/advanced' },
+      ],
+    },
+  ]
 }
